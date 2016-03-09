1052b32b7f34a899a19e222a1eb1f30f4515f52b
Grant Fong <grantchiu011101@gmail.com>
2016-03-10 00:03:25 +0800

    use https remove url

--- test/test_async_require.js
+++ test/test_async_require.js
@@ -1,6 +1,6 @@
 require('../httprequire');
 
-httprequire('http://dl.dropboxusercontent.com/u/368535/async.js',function(async){
+httprequire('https://dl.dropboxusercontent.com/u/368535/async.js',function(async){
 
 	async.each(
 		[11,12,13],
--- test/test_cache.js
+++ test/test_cache.js
@@ -2,16 +2,16 @@ require('../httprequire');
 
 var
 	start = new Date().getTime(),
-	async = httprequire('http://dl.dropboxusercontent.com/u/368535/async.js');
+	async = httprequire('https://dl.dropboxusercontent.com/u/368535/async.js');
 
 console.log("First: ",(new Date().getTime()-start)+" ms");
 
 start = new Date().getTime();
-async = httprequire('http://dl.dropboxusercontent.com/u/368535/async.js');
+async = httprequire('https://dl.dropboxusercontent.com/u/368535/async.js');
 console.log("Second: ",(new Date().getTime()-start)+" ms");
 
 start = new Date().getTime();
-async = httprequire('http://dl.dropboxusercontent.com/u/368535/async.js',true);
+async = httprequire('https://dl.dropboxusercontent.com/u/368535/async.js',true);
 console.log("Third: ",(new Date().getTime()-start)+" ms");
 
 async.each(
